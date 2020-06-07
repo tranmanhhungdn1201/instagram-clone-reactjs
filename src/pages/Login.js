1514957f9b1cd07dff0723334989de063b7d2862
Glitch (starter-react-template) <none>
2020-06-07 09:52:31 +0000

    🏨🎤 Checkpoint ./src/pages/Login.js:16205955/1383

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Row, Col, Form, FormGroup, Label, Input, Button} from 'reactstrap';
 
-const Login = (props) => {
-  onLogin(){
-    
+class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
-  return (
-    <Container>
-      <h1  className="text-center">Login</h1>
-      <Form>
-        <FormGroup row>
-          <Label for="email" sm={2}>Email</Label>
-          <Col sm={10}>
-            <Input type="email" name="email" id="email" placeholder="Email" />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="password" sm={2}>Password</Label>
-          <Col sm={10}>
-            <Input type="password" name="password" id="password" placeholder="Password" />
-          </Col>
-        </FormGroup>
-        <FormGroup className="text-center">
-          <Button onSubmit={this.onLogin}>Đăng nhập</Button>
-        </FormGroup>
-        <Row>
-          <Col className="text-center">
-            <a href="#">Quên mật khẩu?</a>
-            <a href="#">Đăng ký</a>
-          </Col>
-        </Row>
-      </Form>
-    </Container>
-  );
+  handleInputChange(event){
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+  render(){
+    return (
+      <Container>
+        <h1  className="text-center">Login</h1>
+        <Form>
+          <FormGroup row>
+            <Label for="email" sm={2}>Email</Label>
+            <Col sm={10}>
+              <Input type="email" name="email" id="email" placeholder="Email" onChange={this.handleInputChange}/>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="password" sm={2}>Password</Label>
+            <Col sm={10}>
+              <Input type="password" name="password" id="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+          <FormGroup className="text-center">
+            <Button onSubmit={this.onLogin}>Đăng nhập</Button>
+          </FormGroup>
+          <Row>
+            <Col className="text-center">
+              <a href="#">Quên mật khẩu?</a>
+              <a href="#">Đăng ký</a>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default Login;
